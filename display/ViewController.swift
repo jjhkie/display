8e04b753741bcb8a0878cdad80335e86eaa8f7ef
jjhkie <83710866+jjhkie@users.noreply.github.com>
2022-08-18 20:33:08 +0900

    Initial Commit

---
+++ display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  display
+//
+//  Created by 김진혁 on 2022/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
